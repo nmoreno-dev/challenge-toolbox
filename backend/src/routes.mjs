1f2a4afb1567ad8ae4b76ab0ac615c66d79a1fc6
nmoreno <nahuelmoreno.dev@gmail.com>
2024-04-10 13:39:20 -0300

    move logic from router to controller

--- backend/src/routes.mjs
+++ backend/src/routes.mjs
@@ -1,58 +1,10 @@
 import { Router } from 'express';
-import { HttpStatusCode } from 'axios';
-import filesService from './files.service.mjs';
-import logger from './lib/logger.mjs';
+import controller from './controller.mjs';
+
 const appRouter = Router();
 
-appRouter.get('/files/list', async (_, res) => {
-  const data = await filesService.listFiles();
-  if (data === null) {
-    res.status(HttpStatusCode.InternalServerError).json({
-      error: 'Internal Server Error',
-      status: HttpStatusCode.InternalServerError,
-      message: 'Ocurrió un error al obtener la lista de archivos',
-    });
-  }
+appRouter.get('/files/list', controller.getFileList);
 
-  res.status(HttpStatusCode.Ok).json(data);
-});
-
-appRouter.get('/files/data', async (req, res) => {
-  const filesList = await filesService.listFiles();
-  if (filesList === null) {
-    res.status(HttpStatusCode.InternalServerError).json({
-      error: 'Internal Server Error',
-      status: HttpStatusCode.InternalServerError,
-      message: 'Ocurrió un error al obtener la lista de archivos',
-    });
-  }
-
-  const { files } = filesList;
-
-  //   const filesData = files.map(async (fileName) => {
-  //     const fileData = await filesService.downloadFile(fileName);
-  //     if (fileData === null) {
-  //       logger.warn(`Ocurrio un error al obtener la información del archivo ${fileName}`);
-  //     }
-  //     console.log(fileData);
-  //     return fileData;
-  //   });
-
-  const payload = [];
-
-  for (const fileName of files) {
-    const fileData = await filesService.downloadFile(fileName);
-    if (fileData === null) {
-      logger.warn(`Ocurrio un error al obtener la información del archivo ${fileName}`);
-    }
-    console.log(fileData);
-    payload.push({
-      file: fileName,
-      data: fileData,
-    });
-  }
-
-  res.status(HttpStatusCode.Ok).json(payload);
-});
+appRouter.get('/files/data', controller.getFilesData);
 
 export default appRouter;
